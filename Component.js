fcf7fefe699b323e0d6778d3f314cc5a770d74e5
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2021-09-09 08:25:42 -0400

    setState usage

--- Component.js
+++ Component.js
@@ -3,11 +3,19 @@ import { useState } from './MyReact'
 export default function Component({propCount}) {
     // const count = 0
 
-    // can be a primitive value or a function
+    // can be a plain value or a function
     const [count, setCount]= useState(10)
     // const [count, setCount]= useState(() => 10)
 
     const propCountDoubled = 0
+
+    // HACK: we don't have useEffect yet that updates state
+    // update state after 2 seconds
+    setTimeout (() => {
+        // can be plain value or function
+        // setCount(20)
+        setCount(prev => prev + 1)
+    }, 2000)
 
     return `
         State: ${count}
